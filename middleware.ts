470fc98f3db183a611a87d2c0084539f83fb9b0d
Alamatika <302079470+alamatika@users.noreply.github.com>
2026-08-22 12:59:59 +0800

    Fix user login after purchase

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { createServerClient } from "@supabase/ssr";
 import { NextResponse, type NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  const response = NextResponse.next({
+  let response = NextResponse.next({
     request,
   });
 
@@ -14,7 +14,16 @@ export async function middleware(request: NextRequest) {
         getAll() {
           return request.cookies.getAll();
         },
+
         setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value }) => {
+            request.cookies.set(name, value);
+          });
+
+          response = NextResponse.next({
+            request,
+          });
+
           cookiesToSet.forEach(({ name, value, options }) => {
             response.cookies.set(name, value, options);
           });
@@ -23,7 +32,6 @@ export async function middleware(request: NextRequest) {
     }
   );
 
-  // This refreshes the auth session if needed
   await supabase.auth.getUser();
 
   return response;
